ea310579d78128afb2387628801fff0bab2acba2
eliasr124 <elias.rafa@outlook.com>
2022-03-10 00:07:38 -0300

    Add category class

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,4 +1,5 @@
-import { InMemoryDbService } from 'angular-in-memory-web-api'
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Category } from './pages/categories/shared/category.model';
 
 export class InMemoryDataBase implements InMemoryDbService{
     createDb(): any {
