61b5ec0f44c86062ba88ec2eed5a690f9a9ba344
eliasr124 <elias.rafa@outlook.com>
2022-03-10 00:02:01 -0300

    Add im memory database

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,13 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api'
+
+export class InMemoryDataBase implements InMemoryDbService{
+    createDb(): any {
+        const categories = [
+            { id: 1, name: 'Moradia', descripition: 'Pagamentos de contas da casa' },
+            { id: 2, name: 'Saúde', descripition: 'Plano de Saúde e remedio' },
+            { id: 3, name: 'Lazer', descripition: 'cinema,parques, praia, etc' },
+            { id: 4, name: 'Salário', descripition: 'Recebimento de salários' },
+            { id: 5, name: 'Moradia', descripition: 'Pagamentos de contas da casa' }
+        ]
+    }
+}
